13fd30c79b2678b7dad206a7bff15d0d40a71f3b
jerry <jerry@15c49f66-a4e5-453b-9e70-f24e6a7cf82a>
2013-01-16 07:25:39 +0000

    Check-in alternate WiX-based compression program

---
+++ CabResource.cs
@@ -0,0 +1,138 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="muvee Technologies Pte Ltd">
+//   Copyright (c) muvee Technologies Pte Ltd. All rights reserved.
+// </copyright>
+// <author>Jerry Chong</author>
+// --------------------------------------------------------------------------------
+
+namespace stuffer
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Security.Permissions;
+
+    using Microsoft.Deployment.Resources;
+
+    /// <summary>
+    /// Implementation of a Microsoft Cabinet resource
+    /// </summary>
+    internal class CabResource : Resource
+    {
+        #region Kernel32 interops
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UpdateResource(
+            IntPtr updateHandle, string type, IntPtr name, ushort lcid, IntPtr data, uint dataSize);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr BeginUpdateResource(
+            string fileName, [MarshalAs(UnmanagedType.Bool)] bool deleteExistingResources);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EndUpdateResource(
+            IntPtr updateHandle, [MarshalAs(UnmanagedType.Bool)] bool discardChanges);
+
+        #endregion
+
+        /// <summary>
+        /// Defines a new ResourceType for Microsoft Cabinets
+        /// </summary>
+        private static ResourceType Cab
+        {
+            get
+            {
+                return "CABFILE";
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Resource ID to use</param>
+        /// <param name="data">Byte array containing the Microsoft Cabinet</param>
+        public CabResource(string name, ref byte[] data)
+            : base(Cab, name, 0, data)
+        {
+            // nothing extra
+        }
+
+        /// <summary>
+        /// Writes the cabinet resource to a PE binary
+        /// </summary>
+        /// <param name="file">Filesystem path to write to</param>
+        public new void Save(string file)
+        {
+            // require permissions to use file IO
+            new FileIOPermission(FileIOPermissionAccess.AllAccess, file).Demand();
+
+            var updateHandle = IntPtr.Zero;
+            try
+            {
+                // get a handle to begin
+                updateHandle = BeginUpdateResource(file, false);
+                this.Save(updateHandle);
+                if (!EndUpdateResource(updateHandle, false))
+                {
+                    // Win32 API error
+                    int err = Marshal.GetLastWin32Error();
+                    throw new IOException(
+                        string.Format(CultureInfo.InvariantCulture, "Failed to save resource. Error code: {0}", err));
+                }
+
+                updateHandle = IntPtr.Zero;
+            }
+            finally
+            {
+                // free the handle
+                if (updateHandle != IntPtr.Zero)
+                {
+                    EndUpdateResource(updateHandle, true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write byte data to a resource handle
+        /// </summary>
+        /// <param name="updateHandle">Handle obtained from BeginUpdateResource</param>
+        private void Save(IntPtr updateHandle)
+        {
+            var dataPtr = IntPtr.Zero;
+            try
+            {
+                int dataLength = 0;
+                if (this.Data != null)
+                {
+                    //dataLength = this.Data.LongLength;
+                    //dataPtr = Marshal.AllocHGlobal(new IntPtr(dataLength));
+                    dataLength = this.Data.Length;
+                    dataPtr = Marshal.AllocHGlobal(dataLength);
+                    Marshal.Copy(this.Data, 0, dataPtr, dataLength);
+                }
+
+                // initiate updating
+                if (!UpdateResource(
+                        updateHandle,
+                        this.ResourceType.ToString(),
+                        (IntPtr)6699,
+                        (ushort)this.Locale,
+                        dataPtr,
+                        (uint)dataLength))
+                {
+                    throw new IOException("Failed to save resource. Error: " + Marshal.GetLastWin32Error());
+                }
+            }
+            finally
+            {
+                if (dataPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(dataPtr);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="muvee Technologies Pte Ltd">
+//   Copyright (c) muvee Technologies Pte Ltd. All rights reserved.
+// </copyright>
+// <author>Jerry Chong</author>
+// --------------------------------------------------------------------------------
+
+namespace stuffer
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    using Microsoft.Deployment.Compression;
+    using Microsoft.Deployment.Compression.Cab;
+    using Microsoft.Deployment.Resources;
+
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main program entry
+        /// </summary>
+        /// <param name="args">Commandline arguments</param>
+        /// <returns>0 if successful, 1 for errors</returns>
+        public static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: stuffer <directory> <output EXE>");
+                return 1;
+            }
+
+            // backup the target exe to a temporary path
+            var target = Path.GetFullPath(args[1]);
+            var target2 = Path.Combine(Directory.GetParent(target).FullName, "test.exe");
+            File.Copy(target, target2, true);
+
+            // if argument 1 was a .cab file instead of a directory
+            string cabFile;
+            if (File.Exists(args[0]))
+            {
+                var extension = Path.GetExtension(args[0]);
+                Debug.Assert(extension != null);
+                if (extension.ToLower() != ".cab")
+                {
+                    Console.WriteLine("Only cabs are supported: {0}", args[0]);
+                    return 1;
+                }
+
+                cabFile = args[0];
+            }
+            else if (Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Packaging cab... ");
+                cabFile = Path.GetFullPath("test.cab");
+
+                // compress directory into a cabinet
+                var cab = new CabInfo(cabFile);
+                cab.Pack(
+                    args[0], 
+                    true, 
+                    CompressionLevel.Max, 
+                    (source, e) =>
+                    {
+                        if (e.ProgressType == ArchiveProgressType.StartFile)
+                        {
+                            Console.WriteLine("    {0}", e.CurrentFileName);
+                        }
+                    });
+            }
+            else
+            {
+                Console.WriteLine("{0} must be directory or .cab file!", args[0]);
+                return 1;
+            }
+
+            // read to bytes
+            byte[] cabData = File.ReadAllBytes(Path.GetFullPath(cabFile));
+
+            // load resources from exe
+            var rc = new ResourceCollection();
+            rc.Find(target);
+            rc.Load(target);
+
+            // save resources back to exe
+            var r = new CabResource("6699", ref cabData);
+            r.Save(target2);
+            return 0;
+        }
+    }
+}
