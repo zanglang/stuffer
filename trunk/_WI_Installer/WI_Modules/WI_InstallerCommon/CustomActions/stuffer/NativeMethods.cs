17c919ac09631ded339abaeb0e50f035db085fde
jerry <jerry@15c49f66-a4e5-453b-9e70-f24e6a7cf82a>
2013-01-31 06:53:47 +0000

    Rewrite stuffer
    - Support same commandline arguments as stuffIt
    - Support both new and classic bootstrapper compression methods (using "--classic" argument)
    - Use multiple threads for gzip compression

---
+++ trunk/_WI_Installer/WI_Modules/WI_InstallerCommon/CustomActions/stuffer/NativeMethods.cs
@@ -0,0 +1,32 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="NativeMethods.cs" company="Outercurve Foundation">
+//   Copyright (c) 2004, Outercurve Foundation.
+//   This software is released under Microsoft Reciprocal License (MS-RL).
+//   The license and further copyright text can be found in the file
+//   LICENSE.TXT at the root directory of the distribution.
+// </copyright>
+// <summary>
+// Part of the Deployment Tools Foundation project.
+// </summary>
+//---------------------------------------------------------------------
+
+namespace stuffer
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern IntPtr BeginUpdateResource(string fileName, [MarshalAs(UnmanagedType.Bool)] bool deleteExistingResources);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)] [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool UpdateResource(IntPtr updateHandle, string type, string name, ushort lcid, IntPtr data, uint dataSize);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)] [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool UpdateResource(IntPtr updateHandle, string type, IntPtr name, ushort lcid, IntPtr data, uint dataSize);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)] [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EndUpdateResource(IntPtr updateHandle, [MarshalAs(UnmanagedType.Bool)] bool discardChanges);
+    }
+}
